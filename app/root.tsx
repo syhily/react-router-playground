9904ab80977768314053ddeba960b637507599cf
Yufan Sheng <syhily@gmail.com>
2025-03-12 14:05:54 +0800

    feat: remove tailwind and use radix theme.

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "react-router";
+import { Theme } from "@radix-ui/themes";
 
 import type { Route } from "./+types/root";
 import "./app.css";
@@ -33,7 +34,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        {children}
+        <Theme>
+          {children}
+        </Theme>
         <ScrollRestoration />
         <Scripts />
       </body>
